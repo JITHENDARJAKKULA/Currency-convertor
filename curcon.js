d9089ff384854ae52f4a119dc04b3ef8520a8c0c
JITHENDAR JAKKULA <148989477+JITHENDARJAKKULA@users.noreply.github.com>
2024-06-04 12:46:57 +0530

    Add files via upload

---
+++ curcon.js
@@ -0,0 +1,5 @@
+ const BASE_URL= "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json"
+ const dropdown=document.querySelectorAll(".dropdown select");
+ for(code in countryList){
+    console.log(code);
+ }
